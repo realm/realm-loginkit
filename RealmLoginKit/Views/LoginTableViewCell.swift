1e021b14f4db41629c363149936bbc1a99801fa8
Tim Oliver <me@timoliver.com.au>
2017-01-18 16:28:42 +0800

    Added text field to cell

---
+++ RealmLoginKit/Views/LoginTableViewCell.swift
@@ -0,0 +1,50 @@
+//
+//  LoginTableViewCell.swift
+//  RealmLoginKit
+//
+//  Created by Tim Oliver on 1/18/17.
+//  Copyright © 2017 Realm. All rights reserved.
+//
+
+import UIKit
+import TORoundedTableView
+
+class LoginTableViewCell: TORoundedTableViewCapCell, UITextFieldDelegate {
+    
+    let textField = UITextField()
+    var textChangedHandler: (() -> Void)?
+    
+    //MARK: - Class Creation
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setUpViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Text Field Setup
+    
+    func setUpViews() {
+        textField.delegate = self
+        textField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        contentView.addSubview(textField)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        // The table cell's separatorInset property has only just been updated to the
+        // final value at this point, so re-align the text field to match
+        textField.frame = contentView.frame
+        textField.frame.origin.x = separatorInset.left
+        textField.frame.size.width -= separatorInset.left
+    }
+    
+    //MARK: - Text Field Delegate
+    @objc private func textFieldDidChange(_ sender: AnyObject?) {
+        textChangedHandler?()
+    }
+}
