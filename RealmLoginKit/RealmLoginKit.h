6754900196dc3d387ed603ec7b34f4e2281e1cbe
Tim Oliver <me@timoliver.com.au>
2017-01-20 21:29:08 +0800

    Added dynamic framework

---
+++ RealmLoginKit/RealmLoginKit.h
@@ -0,0 +1,19 @@
+//
+//  RealmLoginKit.h
+//  RealmLoginKit
+//
+//  Created by Tim Oliver on 1/20/17.
+//  Copyright © 2017 Realm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RealmLoginKit.
+FOUNDATION_EXPORT double RealmLoginKitVersionNumber;
+
+//! Project version string for RealmLoginKit.
+FOUNDATION_EXPORT const unsigned char RealmLoginKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RealmLoginKit/PublicHeader.h>
+
+
