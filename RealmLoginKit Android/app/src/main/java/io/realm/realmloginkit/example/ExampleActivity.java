2a42cb18847be88c498d6078c3b530d9487cf32c
LYK <dalinaum@gmail.com>
2017-02-21 21:47:50 +0900

    Add RealmLogoView.

---
+++ RealmLoginKit Android/app/src/main/java/io/realm/realmloginkit/example/ExampleActivity.java
@@ -0,0 +1,13 @@
+package io.realm.realmloginkit.example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example);
+    }
+}
