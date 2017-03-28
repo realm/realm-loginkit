73cca1ab1e5e8981628201a60183f7ccfdc4c048
Tim Oliver <me@timoliver.com.au>
2017-03-28 15:46:19 -0700

    Integrated tests into CocoaPods

--- RealmLoginKit Apple/RealmLoginKitExampleTests/RealmLoginKitExampleTests.swift
+++ RealmLoginKit Apple/RealmLoginKitExampleTests/RealmLoginKitExampleTests.swift
@@ -7,22 +7,13 @@
 //
 
 import XCTest
+@testable import RealmLoginKitExample
 
 class RealmLoginKitExampleTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
+
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let controller = LoginViewController()
+        XCTAssertNotNil(controller)
     }
     
     func testPerformanceExample() {
