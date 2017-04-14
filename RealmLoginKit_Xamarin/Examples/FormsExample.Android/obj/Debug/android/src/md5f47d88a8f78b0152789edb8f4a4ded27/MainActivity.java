a4d41e463e8398069b7385dd5ea32cf27b1b52f9
Andy Dent <dentaroo@gmail.com>
2017-04-14 10:52:46 +0800

    Moved subtree up and renamed projects to get paths to levels VS2017 will build

---
+++ RealmLoginKit_Xamarin/Examples/FormsExample.Android/obj/Debug/android/src/md5f47d88a8f78b0152789edb8f4a4ded27/MainActivity.java
@@ -0,0 +1,47 @@
+package md5f47d88a8f78b0152789edb8f4a4ded27;
+
+
+public class MainActivity
+	extends md5b60ffeb829f638581ab2bb9b1a7f4f3f.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("FormsExample.Droid.MainActivity, FormsExample.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("FormsExample.Droid.MainActivity, FormsExample.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
