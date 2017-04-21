1d48608dc88a7e0d6c305de8249eaebd8ccbc9fd
Tim Oliver <me@timoliver.com.au>
2017-04-20 22:45:37 -0700

    Added tests for table data source

--- RealmLoginKit Apple/RealmLoginKitExampleTests/LoginViewControllerTests.swift
+++ RealmLoginKit Apple/RealmLoginKitExampleTests/LoginViewControllerTests.swift
@@ -9,7 +9,7 @@
 import XCTest
 @testable import RealmLoginKitExample
 
-class RealmLoginKitViewControllerTests: XCTestCase {
+class LoginViewControllerTests: XCTestCase {
 
     func testLoginControllerCreation() {
         let controller = LoginViewController()
