b3dad2f194bededf2b815e3931799d89768c59b1
Tim Oliver <me@timoliver.com.au>
2017-05-02 17:29:15 -0700

    Improvements to auth provider

--- RealmLoginKit Apple/RealmLoginKit/Models/AuthenticationProviders/AWSCognitoAuthenticationProvider.swift
+++ RealmLoginKit Apple/RealmLoginKit/Models/AuthenticationProviders/AWSCognitoAuthenticationProvider.swift
@@ -32,6 +32,7 @@ class AWSCognitoAuthenticationProvider: NSObject, AuthenticationProvider, AWSCog
     private let serviceRegion: AWSRegionType
     private let userPoolID: String
     private let clientID: String
+    private let clientSecret: String
     private let userPool: AWSCognitoIdentityUserPool
 
     // Task token to let us cancel requests as needed
@@ -42,10 +43,11 @@ class AWSCognitoAuthenticationProvider: NSObject, AuthenticationProvider, AWSCog
         self.serviceRegion = serviceRegion
         self.userPoolID = userPoolID
         self.clientID = clientID
+        self.clientSecret = clientSecret
 
         // Access the User Pool object containing our users
         let serviceConfiguration = AWSServiceConfiguration(region: self.serviceRegion, credentialsProvider: nil)
-        let poolConfiguration = AWSCognitoIdentityUserPoolConfiguration(clientId: self.clientID, clientSecret: clientSecret, poolId: self.userPoolID)
+        let poolConfiguration = AWSCognitoIdentityUserPoolConfiguration(clientId: self.clientID, clientSecret: self.clientSecret, poolId: self.userPoolID)
         AWSCognitoIdentityUserPool.register(with: serviceConfiguration, userPoolConfiguration: poolConfiguration, forKey:"RealmLoginKit")
         self.userPool = AWSCognitoIdentityUserPool(forKey: "RealmLoginKit")
 
@@ -82,7 +84,7 @@ class AWSCognitoAuthenticationProvider: NSObject, AuthenticationProvider, AWSCog
         // The block called when the response from the user session request is complete
         let getUserSessionBlock: ((AWSTask<AWSCognitoIdentityUserSession>) -> Void) = { task in
             if (task.error != nil) {
-                onCompletion?(nil, task.error!)
+                onCompletion?(nil, self.formattedError(task.error! as NSError))
                 return
             }
 
@@ -93,7 +95,7 @@ class AWSCognitoAuthenticationProvider: NSObject, AuthenticationProvider, AWSCog
         // The block called when the response from a signup request is received
         let signUpBlock: ((AWSTask<AWSCognitoIdentityUserPoolSignUpResponse>) -> Void) = { task in
             if (task.error != nil) {
-                onCompletion?(nil, task.error!)
+                onCompletion?(nil, self.formattedError(task.error! as NSError))
                 return
             }
 
@@ -115,7 +117,7 @@ class AWSCognitoAuthenticationProvider: NSObject, AuthenticationProvider, AWSCog
         // Set up the block that will be called when we get a response from the server
         let getUserSessionBlock: ((AWSTask<AWSCognitoIdentityUserSession>) -> Void) = { task in
             if (task.error != nil) {
-                onCompletion?(nil, task.error!)
+                onCompletion?(nil, self.formattedError(task.error! as NSError))
                 return
             }
 
@@ -132,5 +134,14 @@ class AWSCognitoAuthenticationProvider: NSObject, AuthenticationProvider, AWSCog
             DispatchQueue.main.async { getUserSessionBlock(task) }
             return nil
         }, cancellationToken: cancellationTokenSource!.token)
+    }
+
+    // Cognito returns the error message in a "message" property in
+    // 'userInfo'. This method copies that string to 'localizedDescription' 
+    // for easier access
+    private func formattedError(_ error: NSError) -> NSError {
+        var userInfo = error.userInfo
+        userInfo[NSLocalizedDescriptionKey] = userInfo["message"]
+        return NSError(domain: error.domain, code: error.code, userInfo: userInfo)
     }
 }
